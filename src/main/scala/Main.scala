f60e71b65d3b20811faa5fc83f3edc439b29cd2e
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-11-16 18:55:54 +0900

    特定のユーザーが発言した場合のみツイートを送信する

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,8 @@ import scala.concurrent.ExecutionContextExecutor
 
 object Main extends App {
   val token = sys.env("SLACK_TOKEN")
+  val operatableUserId = sys.env("OPERATABLE_USER_ID")
+
   implicit val system: ActorSystem = ActorSystem("slack")
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
@@ -18,10 +20,12 @@ object Main extends App {
     if (selfId == message.user) {
       // do not reaction
     } else if (mentionedIds.contains(selfId)) {
-      try {
-        new TwitterFactory().getInstance().updateStatus(message.text.substring(13))
-      } catch {
-        case e: Exception => client.sendMessage(message.channel, "ツイート送信失敗")
+      if (message.user == operatableUserId) {
+        try {
+          new TwitterFactory().getInstance().updateStatus(message.text.substring(13))
+        } catch {
+          case e: Exception => client.sendMessage(message.channel, "ツイート送信失敗")
+        }
       }
     }
   }
