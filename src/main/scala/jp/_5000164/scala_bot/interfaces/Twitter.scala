6657be6a96c62ec9f8afaa7cdc493f77083355da
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-11-27 20:20:14 +0900

    コード整形

--- src/main/scala/jp/_5000164/scala_bot/interfaces/Twitter.scala
+++ src/main/scala/jp/_5000164/scala_bot/interfaces/Twitter.scala
@@ -3,15 +3,15 @@ package jp._5000164.scala_bot.interfaces
 import twitter4j.TwitterFactory
 
 /**
- * Twitter とやりとりをする
- */
+  * Twitter とやりとりをする
+  */
 class Twitter {
   /**
-   * ツイートする
-   *
-   * @param text ツイートする内容
-   * @return ツイートに成功したかどうか
-   */
+    * ツイートする
+    *
+    * @param text ツイートする内容
+    * @return ツイートに成功したかどうか
+    */
   def tweet(text: String): Boolean = {
     try {
       new TwitterFactory().getInstance().updateStatus(text)
