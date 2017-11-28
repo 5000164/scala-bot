d7e2d62b1bf394dc1f52116ae19c3d28248faccb
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-11-28 14:02:00 +0900

    特定の時だけ反応することを確認するテストを追加

--- src/test/scala/jp/_5000164/scala_bot/interfaces/OperatorSpec.scala
+++ src/test/scala/jp/_5000164/scala_bot/interfaces/OperatorSpec.scala
@@ -1,6 +1,6 @@
 package jp._5000164.scala_bot.interfaces
 
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.{never, verify}
 import org.scalatest.FreeSpec
 import org.scalatest.mockito.MockitoSugar
 import slack.models.Message
@@ -22,5 +22,39 @@ class OperatorSpec extends FreeSpec with MockitoSugar {
     operator.run(message)
 
     verify(mockTwitter).tweet("text")
+  }
+
+  "bot にメンションされていない時は反応しない" in {
+    val mockClient = mock[SlackRtmClient]
+    val mockTwitter = mock[Twitter]
+    val operator = new Operator(mockClient, "AAAAAAAAA", "BBBBBBBBB", mockTwitter)
+    val message = new Message(
+      "ts",
+      "channel",
+      "BBBBBBBBB",
+      "ttt text",
+      None,
+      None
+    )
+    operator.run(message)
+
+    verify(mockTwitter, never).tweet("text")
+  }
+
+  "操作が許可されたユーザーではない時は反応しない" in {
+    val mockClient = mock[SlackRtmClient]
+    val mockTwitter = mock[Twitter]
+    val operator = new Operator(mockClient, "AAAAAAAAA", "BBBBBBBBB", mockTwitter)
+    val message = new Message(
+      "ts",
+      "channel",
+      "CCCCCCCCC",
+      "<@AAAAAAAAA> ttt text",
+      None,
+      None
+    )
+    operator.run(message)
+
+    verify(mockTwitter, never).tweet("text")
   }
 }
